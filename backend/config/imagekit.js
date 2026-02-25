510d145e5df0070aa053b083b6f51d0627b9eed2
Yogesh <yogeshkawar746@gmail.com>
2026-02-26 01:41:39 +0530

    imagekit.js added

---
+++ backend/config/imagekit.js
@@ -0,0 +1,11 @@
+const ImageKit = require("imagekit");
+require('dotenv').config();
+
+// Initialize ImageKit with your environment variables
+const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
+module.exports = imagekit;
